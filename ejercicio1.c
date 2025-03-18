1f4c6f007942c64e13615464f3b61ad02b6bb5a1
Millovoyman <millovichfm@gmail.com>
2025-03-17 21:19:48 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+// Emilio Flores Mejía
+// Ejercicio 1
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+    {
+        printf("a es mayor \n");
+    }
+    return 0;
+}
