46c6bbf1650474ef042a507deeb94967c5bc4b49
Millovoyman <millovichfm@gmail.com>
2025-03-17 21:22:21 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,18 @@
+// Emilio Flores Mejía
+// Ejercicio 2
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a=3;
+    int b=4;
+    if(a>b)
+    {
+        printf("a es mayor \n");
+    }
+    else
+    {
+        printf("b es mayor o igual a a \n");
+    }
+    return 0;
+}
